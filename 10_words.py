dcd32ce0d8b59519e1871338daf109ad5606da05
Nikolaos Douras <ndouras@deloitte.gr>
2024-02-28 13:43:29 +0200

    adding the ability to say happy birthday if it is the persons birthday

--- 10_words.py
+++ 10_words.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 data = [
     ("Emily", "1990-05-15"),
     ("Alexander", "1985-10-25"),
@@ -6,12 +8,17 @@ data = [
     ("Olivia", "1995-07-03"),
     ("William", "1982-09-12"),
     ("Isabella", "2002-11-30"),
-    ("James", "1970-02-22"),
+    ("James", "1970-02-28"),
     ("Charlotte", "1988-06-10"),
     ("Benjamin", "1999-04-05")
 ]
 
+# Get today's month and day
+today_month_day = datetime.now().strftime("%m-%d")
 
 for person in data:
-    print(f"Hello {person[0]}! You were born on {person[1]}")
-
+    name, birthdate = person
+    if today_month_day == birthdate[5:]:
+        print(f"Hello {name}! Happy birthday!!!!!!!!!!!!!!!!!!!!! You were born on {birthdate}")
+    else:
+        print(f"Hello {name}! You were born on {birthdate}")
