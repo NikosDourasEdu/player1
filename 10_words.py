837552486a07d74ca38ec1934787a2ba0720d06a
Nikolaos Douras <ndouras@deloitte.gr>
2024-02-28 13:10:13 +0200

    Added Birthdates and a new message

--- 10_words.py
+++ 10_words.py
@@ -1,5 +1,17 @@
-names = ["Emily", "Alexander", "Sophia", "Liam", "Olivia", "William", "Isabella", "James", "Charlotte", "Benjamin"]
+data = [
+    ("Emily", "1990-05-15"),
+    ("Alexander", "1985-10-25"),
+    ("Sophia", "2000-03-08"),
+    ("Liam", "1978-12-18"),
+    ("Olivia", "1995-07-03"),
+    ("William", "1982-09-12"),
+    ("Isabella", "2002-11-30"),
+    ("James", "1970-02-22"),
+    ("Charlotte", "1988-06-10"),
+    ("Benjamin", "1999-04-05")
+]
 
-for name in names:
-    print(f"Hello {name}")
+
+for person in data:
+    print(f"Hello {person[0]}! You were born on {person[1]}")
 
